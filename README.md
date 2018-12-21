4d97d233179d167defd67d9182b9f1f9d9277ec8
agentRaccoon <hobin.park90@gmail.com>
2018-12-21 11:26:02 +0900

    2nd tutorial for commit

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ hi, there
 
 Hi, all this is Evan Park.
 Raccoon is one of the hardest animal in the world.
+Don't you think so?
--- hithere.py
+++ hithere.py
@@ -1,2 +1,3 @@
 print("hello world")
 print("hello next line for git commit tutorial")
+print("life is hard stuff. no one can fully understand oneself")
